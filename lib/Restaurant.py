85d53ef55328ea958c75d5c531cf7fdd1a1ac9ef
tylerkirumba <Tyler>
2023-05-30 15:59:03 +0300

    Restaurant

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,2 +1,16 @@
 class Restaurant:
+    # Class variable to store all restaurant instances
+    all_restaurants = [] 
+
+     #Restaurants should be initialized with a name, as a string
+    def __init__(self, name):
+        self.name = name
+        Restaurant.all_restaurants.append(self)
+
+     #returns the restaurant's name 
+    def name(self):
+        return self.name
+    
   
+
+    
--- lib/Review.py
+++ lib/Review.py
@@ -1,2 +1,2 @@
 class Review:
-    pass
+   pass
