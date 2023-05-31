47bb2095518c6cf1949b7fbce6c570e0bf85fedb
tylerkirumba <Tyler>
2023-05-31 09:46:22 +0300

    progess

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,3 +1,5 @@
+from Review import Review
+
 class Restaurant:
     # Class variable to store all restaurant instances
     all_restaurants = [] 
@@ -10,6 +12,21 @@ class Restaurant:
      #returns the restaurant's name 
     def name(self):
         return self.name
+    
+    # restaurant review
+    def reviews(self):
+        return [review for review in Review.all() if review.restaurant() == self] 
+
+    def customers(self):
+        return list(set([review.customer() for review in self.reviews()]))
+    
+    def average_star_rating(self):
+        ratings = [review.rating() for review in self.reviews()]
+        if ratings:
+            return sum(ratings) / len(ratings)
+        else:
+            return None
+ 
     
   
 
